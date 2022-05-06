032122f33b152d39be050e9e025c34f56a8600f2
widad123 <khatiri.widad@gmail.com>
2022-05-06 15:01:19 +0200

    hashage function sha256

--- BlockchainFundamentals/BlockchainBasics/hashing.js
+++ BlockchainFundamentals/BlockchainBasics/hashing.js
@@ -1,4 +1,45 @@
-var SHA256 = require("crypto-js/sha256");
 
-const sha256 = SHA256('Udacity');
-console.log(sha256)
+/**
+ * Step 1: Verify 'crypto-js' module is installed for your project.
+ * After verifying it is installed, move on to Step 2. No code needs to be written here.
+ */
+
+/**
+ * Step 2: Import from crypto-js module the SHA256 library.
+ */
+
+// Write your code here
+
+/**
+ * Variables: Do not change variable values.
+ */
+
+ var SHA256 = require("crypto-js/sha256");
+ //const sha256 = SHA256('Udacity');
+ //console.log(sha256)
+
+ const data1 = "Blockchain Rock!";
+ const dataObject = {
+     id: 1,
+       body: "With Object Works too",
+       time: new Date().getTime().toString().slice(0,-3)
+ };
+ 
+ /**
+  * Step 3: Add code to the `generate hash function
+  * Function that generates the SHA256 Hash
+  * @param {*} obj 
+  */
+ 
+ function generateHash(obj) {
+    const sha256 = SHA256(JSON.stringify(obj));
+    return sha256
+}
+ 
+ console.log(`SHA256 Hash: ${generateHash(data1)}`);
+ console.log("************************************");
+ console.log(`SHA256 Hash: ${generateHash(dataObject)}`);
+ 
+ /**
+  * Run your application using `node hashing.js`
+  */
