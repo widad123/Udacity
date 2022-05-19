d9f4979a2384c23494412c8d4a31ee63dcc1b335
widad123 <khatiri.widad@gmail.com>
2022-05-19 13:56:23 +0200

    exo first blockchain

---
+++ BlockchainFundamentals/BlockchainBasics/tx.js
@@ -0,0 +1,6 @@
+//createrawtransaction '[{"txid":"TXID","vout":VOUT}]' '{"TO ADDRESS":amount,"FROM ADDRESS":amount}'
+
+//createrawtransaction'[{"txid":"626f7e516858eb44d56c160b4807008dd4633e4ac36e410b1c1cd98703ed94e6","vout":0}]''{"tb1qjgl94766h850mdjfwjyhuyrx99zzsddtzzfaxd":0.006,"tb1qdygv73fzk0960c2flhcp05efzndyhqv5lk2m5k":0.00062}'
+
+
+//createrawtransaction  '[{"txid":"626f7e516858eb44d56c160b4807008dd4633e4ac36e410b1c1cd98703ed94e6","vout": 0}]' '{"tb1q6zxhtyktup7kgg5n0y7kcdjh4wmax2y07mhghs":0.0001,"tb1qdygv73fzk0960c2flhcp05efzndyhqv5lk2m5k":0.0003}'
